fff101aa7e5fe3a154e016f552fdd92bfa3f528e
Julian Jark <juliuxu@gmail.com>
2023-06-09 20:07:26 +0200

    :lipstick: add logo

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import manifest from "~/assets/manifest.webmanifest";
+import svgLogo from "~/assets/logo.svg";
+import pngLogo from "~/assets/logo.png";
 import fontComico from "~/styles/font-comico.css";
 import fontSatoshi from "~/styles/font-satoshi.css";
 import tailwind from "~/styles/tailwind.css";
@@ -27,12 +30,25 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: fontSatoshi,
   },
+  {
+    rel: "icon",
+    href: svgLogo,
+  },
+  {
+    rel: "apple-touch-icon",
+    href: pngLogo,
+  },
+  {
+    rel: "manifest",
+    href: manifest,
+  },
 ];
 
 export default function App() {
   return (
     <html lang="no">
       <head>
+        <meta name="theme-color" content="#F9A613" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
