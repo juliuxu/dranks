95684662f0b30f83c99be3c0ffcf1e0eb2d6f105
Julian Jark <juliuxu@gmail.com>
2023-06-11 00:18:36 +0200

    fix list

--- app/routes/_layout/header.tsx
+++ app/routes/_layout/header.tsx
@@ -13,61 +13,74 @@ export const Header = () => {
     <header>
       <nav className={`${dranksClasses.layoutPadding}`}>
         <ul className="flex min-h-[5rem] flex-wrap items-center gap-y-4 gap-x-8 py-4">
-          <NavLink
-            prefetch="intent"
-            to="/"
-            end
-            className={({ isActive }) =>
-              `${classes.link} ${isActive && classes.linkActive}`
-            }
-          >
-            Dranks
-          </NavLink>
-          <NavLink
-            prefetch="intent"
-            to="/sirup"
-            className={({ isActive }) =>
-              `${classes.link} ${isActive && classes.linkActive}`
-            }
-          >
-            Sirup
-          </NavLink>
-          <NavLink
-            prefetch="intent"
-            to="/super-juice"
-            className={({ isActive }) =>
-              `${classes.link} ${isActive && classes.linkActive}`
-            }
-          >
-            Super juice
-          </NavLink>
-          <NavLink
-            prefetch="intent"
-            to="/utstyr"
-            className={({ isActive }) =>
-              `${classes.link} ${isActive && classes.linkActive}`
-            }
-          >
-            Utstyr
-          </NavLink>
+          <li>
+            <NavLink
+              prefetch="intent"
+              to="/"
+              end
+              className={({ isActive }) =>
+                `${classes.link} ${isActive && classes.linkActive}`
+              }
+            >
+              Dranks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              prefetch="intent"
+              to="/sirup"
+              className={({ isActive }) =>
+                `${classes.link} ${isActive && classes.linkActive}`
+              }
+            >
+              Sirup
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              prefetch="intent"
+              to="/super-juice"
+              className={({ isActive }) =>
+                `${classes.link} ${isActive && classes.linkActive}`
+              }
+            >
+              Super juice
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              prefetch="intent"
+              to="/utstyr"
+              className={({ isActive }) =>
+                `${classes.link} ${isActive && classes.linkActive}`
+              }
+            >
+              Utstyr
+            </NavLink>
+          </li>
 
-          <div className="h-0 flex-grow-0 basis-full md:flex-grow md:basis-auto" />
-          <div className="flex w-full flex-col gap-y-4 gap-x-5 md:w-auto md:flex-row lg:gap-x-8">
-            <Link
-              prefetch="intent"
-              to="/quiz"
-              className={`${classes.linkButton} w-full bg-dranks-orange text-white md:w-40`}
-            >
-              Quiz
-            </Link>
-            <Link
-              prefetch="intent"
-              to="/last-ned-app"
-              className={`${classes.linkButton} w-full bg-light-orange md:w-40`}
-            >
-              Last ned app
-            </Link>
-          </div>
+          <li
+            aria-hidden
+            className="h-0 flex-grow-0 basis-full md:flex-grow md:basis-auto"
+          />
+          <li className="contents">
+            <ul className="flex w-full flex-col gap-y-4 gap-x-5 md:w-auto md:flex-row lg:gap-x-8">
+              <Link
+                prefetch="intent"
+                to="/quiz"
+                className={`${classes.linkButton} w-full bg-dranks-orange text-white md:w-40`}
+              >
+                Quiz
+              </Link>
+              <Link
+                prefetch="intent"
+                to="/last-ned-app"
+                className={`${classes.linkButton} w-full bg-light-orange md:w-40`}
+              >
+                Last ned app
+              </Link>
+            </ul>
+          </li>
         </ul>
       </nav>
     </header>
