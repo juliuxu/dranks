8b7c264798c02461cc9521d1b5b503225e9ec529
Julian Jark <juliuxu@gmail.com>
2023-06-10 20:40:17 +0200

    :zap: set auto format
    will choose webp if possible, which for most cases is the best format

--- app/routes/api.notion-image.ts
+++ app/routes/api.notion-image.ts
@@ -15,7 +15,7 @@ const imageUrlBuilder = createImageUrlBuilder(
  * Browser -> Cloudinary -> This API -> Notion
  */
 const cloudinaryImageUrlBuilder: typeof imageUrlBuilder = (...args) =>
-  `https://res.cloudinary.com/dxv2fa2wh/image/fetch/q_auto/${encodeURIComponent(
+  `https://res.cloudinary.com/dxv2fa2wh/image/fetch/f_auto,q_auto/${encodeURIComponent(
     imageUrlBuilder(...args)
   )}`;
 export { cloudinaryImageUrlBuilder as imageUrlBuilder };
